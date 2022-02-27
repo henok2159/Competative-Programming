97ec05b1f4db7b3cba659eedc4ee1514940088fb
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-02-27 16:02:18 +0300

    Create dominoPilling.java

---
+++ dominoPilling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+
+	public static void main(String[] args) {
+
+		Scanner sc  = new Scanner(System.in);
+		int n  = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println(m*n/2);
+	}
+}
