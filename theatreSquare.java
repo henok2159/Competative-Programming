9f04be6a925e7a3536ff87de9d418a5a9dc40248
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-03-01 20:20:00 +0300

    Create theatreSquare.java

---
+++ theatreSquare.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+ 
+public class  {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner sc  = new Scanner(System.in);
+		double n  = sc.nextDouble();
+		double m = sc.nextDouble();
+		double a = sc.nextDouble();
+		int a1=(int)Math.ceil(n/a);
+		int a2=(int)Math.ceil(m/a);
+		System.out.println(a1*a2);
+	}
+}
