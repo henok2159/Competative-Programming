a95ded13b86e04fac3a904d24f4c59a4d208f5e6
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-04-06 16:00:54 +0300

    Create containerWithMostWater.java

---
+++ containerWithMostWater.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea=0;
+       int i=0;
+        int j=height.length-1;
+        while(i<j){
+            maxArea=Math.max(maxArea,(j-i)*Math.min(height[i],height[j]));
+            if(height[i]<height[j]){
+                i++;
+            }else j--;
+        }
+       return maxArea;
+    }
+}
