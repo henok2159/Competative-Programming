b648fbb18a87d635485c9c4d8ff46928587fb854
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-06-29 11:34:44 +0300

    Create ksmallest.java

---
+++ ksmallest.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n=matrix.length;
+       PriorityQueue<Integer>pq=new PriorityQueue();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                pq.add(matrix[i][j]);
+            }
+        }
+        for(int i=0;i<k-1;i++){
+            pq.poll();
+        }
+      return  pq.poll();
+    }
+}
