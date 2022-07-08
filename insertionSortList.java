6762039430b063dfb32f246e80ba16fef279bd17
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-07-08 11:16:37 +0300

    Create insertionSortList.java

---
+++ insertionSortList.java
@@ -0,0 +1,18 @@
+public ListNode insertionSortList(ListNode head) {
+        ListNode current=head;
+        List<Integer> list=new ArrayList<>();
+        while(current!=null){
+            list.add(current.val);
+            current=current.next;
+        }
+        Collections.sort(list);
+        ListNode c2=head;
+        int i=0;
+        while(c2!=null&&i<list.size()){
+            c2.val=list.get(i);
+            c2=c2.next;
+            i++;
+        }
+        return head;
+    }
+Footer
