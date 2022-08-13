c29b3822fb7235739b40ecd37bda1650aec2f681
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-08-13 15:33:14 +0300

    Update ksmallest.java

--- ksmallest.java
+++ ksmallest.java
@@ -14,3 +14,22 @@ class Solution {
       return  pq.poll();
     }
 }
+//modified
+class Solution {
+    public int kthSmallest(int[][] matrix, int k) {
+        int n=matrix.length;
+        int count=0;
+        PriorityQueue<Integer>pq=new PriorityQueue(Collections.reverseOrder());
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(count++<k){
+                    pq.add(matrix[i][j]);
+                }else{
+                    pq.add(matrix[i][j]);
+                   pq.remove();
+                }
+            }
+        }
+        return pq.remove();
+    }
+}
