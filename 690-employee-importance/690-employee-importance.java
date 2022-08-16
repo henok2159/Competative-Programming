3fb0d55729d8bbb204045e169e5c123174292d95
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-08-16 15:24:48 +0300

    Time: 8 ms (75.87%), Space: 58.8 MB (30.90%) - LeetHub

--- 690-employee-importance/690-employee-importance.java
+++ 690-employee-importance/690-employee-importance.java
@@ -10,23 +10,20 @@ class Employee {
 class Solution {
     int importance=0;
     public int getImportance(List<Employee> employees, int id) {
+        HashMap<Integer,Employee>hm=new HashMap();
         for(Employee em:employees){
-            if(em.id==id){
-                findImportance(employees,em);
-                break;
+           hm.put(em.id,em);
             }
-        }
+        findImportance(hm,hm.get(id));
+        
         return importance;
     }
     
-    int findImportance(List<Employee>employees,Employee em){
+    int findImportance(HashMap<Integer,Employee>hm,Employee em){
        importance+=em.importance ;
+        
         for(int sub:em.subordinates){
-            for(Employee e:employees){
-                if(e.id==sub){
-                    findImportance(employees,e);
-                }
-            }
+          findImportance(hm,hm.get(sub));
         }
         return importance;
     }
