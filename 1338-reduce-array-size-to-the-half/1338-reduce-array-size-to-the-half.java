4571d4c95ba7d53dc65fb134826d60631c3fb525
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-08-18 06:37:45 +0300

    Time: 444 ms (5.02%), Space: 108.2 MB (5.25%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minSetSize(int[] arr) {
+       HashMap<Integer,Integer>hm=new HashMap();
+        PriorityQueue<Integer>pq=new PriorityQueue(Collections.reverseOrder());
+        int n=arr.length;
+        for(int num:arr){
+        
+            hm.put(num,hm.getOrDefault(num,0)+1);
+        }
+    for(Map.Entry<Integer,Integer>m:hm.entrySet()){
+        pq.add(m.getValue());
+    }
+      double d=0;
+        int count=0;
+        while(d<n/2){
+            count++;
+            d+=pq.remove();
+            System.out.print(d+" ");
+        }
+        return count;
+    }
+}
