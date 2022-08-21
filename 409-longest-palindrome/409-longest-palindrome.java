8cfe8ae433563f376f67d0552865b15e24a6a0f2
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-08-21 12:12:34 +0300

    Time: 12 ms (15.79%), Space: 40.7 MB (90.80%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -6,13 +6,17 @@ class Solution {
         
         for(int i=0;i<n;i++){
             hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+            if(hm.get(s.charAt(i))==2){
+                longest+=2;
+                hm.put(s.charAt(i),0);
+            }
         }
-        for(int i=0;i<n;i++){
-           int rep= hm.get(s.charAt(i));
-            hm.put(s.charAt(i),0);
-            if(rep%2==0)longest+=rep;
-            else longest+=rep-1;
-        }
+        // for(int i=0;i<n;i++){
+        //    int rep= hm.get(s.charAt(i));
+        //     hm.put(s.charAt(i),0);
+        //     if(rep%2==0)longest+=rep;
+        //     else longest+=rep-1;
+        // }
         if(longest<n)
             return longest+1;
         else return longest;
