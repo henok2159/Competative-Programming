68da035ebd79b6c12c1e21aeea9e3e65fb666519
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2022-08-21 12:33:26 +0300

    Time: 1 ms (100.00%), Space: 40.7 MB (92.65%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -1,24 +1,14 @@
 class Solution {
     public int longestPalindrome(String s) {
-        int n=s.length();
-        int longest=0;
-        HashMap<Character,Integer>hm=new HashMap();
-        
-        for(int i=0;i<n;i++){
-            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
-            if(hm.get(s.charAt(i))==2){
-                longest+=2;
-                hm.put(s.charAt(i),0);
-            }
+        int[] count = new int[128];
+        for (char c: s.toCharArray())
+            count[c]++;
+
+        int ans = 0;
+        for (int v: count) {
+            ans += v / 2 * 2;
+            
         }
-        // for(int i=0;i<n;i++){
-        //    int rep= hm.get(s.charAt(i));
-        //     hm.put(s.charAt(i),0);
-        //     if(rep%2==0)longest+=rep;
-        //     else longest+=rep-1;
-        // }
-        if(longest<n)
-            return longest+1;
-        else return longest;
+        return ans<s.length()?ans+1:ans;
     }
 }
