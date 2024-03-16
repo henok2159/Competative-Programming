15c0e4ca781fc5b7079fbbc6b5318c4e24f8a13f
Henok Mitku <81478651+henok2159@users.noreply.github.com>
2024-03-16 14:33:58 +0300

    Time: 250 ms (9.30%), Space: 23.5 MB (18.92%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -0,0 +1,29 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        
+        close_penality=[0]*(len(customers)+1)
+        open_penality=[0]*(len(customers)+1)
+        
+        for i in range(len(customers)):
+            if customers[i]=='N':
+                open_penality[i+1]=open_penality[i]+1
+            else:
+                open_penality[i+1]=open_penality[i]
+        
+        for i in range(len(customers)-1,-1,-1):
+            if customers[i]=='Y':
+                close_penality[i]=close_penality[i+1]+1
+            else:
+                close_penality[i]=close_penality[i+1]
+        
+        mx=111234567897652345678
+        pos=11111111111111111111
+        print(open_penality)
+        print(close_penality)
+        for i in range(len(close_penality)):
+            if mx>close_penality[i]+open_penality[i]:
+                mx=close_penality[i]+open_penality[i]
+                pos=i
+        return pos
+            
+        
